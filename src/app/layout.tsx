2274e14697bbb20951e5a210e74d17d5522370ab
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2025-02-19 00:08:38 -0300

    feat: :tada: start price my pc project

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import theme from "@/theme";
+import { ThemeProvider } from "@mui/material";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Home page PriceMyPc",
+  description: "Site to review the value of your pc",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import "@fontsource/geist/300.css";
+import "@fontsource/geist/400.css";
+import "@fontsource/geist/500.css";
+import "@fontsource/geist/700.css";
+import { Box, Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
+      <Typography>PRICE MY PC</Typography>
+    </Box>
+  );
+}
